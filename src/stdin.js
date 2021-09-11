091fa6f45999acbf3af23f77959c735829026c14
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-09-11 08:15:53 -0300

    Handling incomplete lines from stdin data

--- src/stdin.js
+++ src/stdin.js
@@ -1,12 +1,17 @@
 const Podium = require("@hapi/podium");
 
 const stdin = new Podium('log-line');
+let incompleteLine = '';
 
-process.stdin.on('data', (data) => {
-    data.toString()
-        .split('\n')
-        .filter(l => l.length > 0)
-        .forEach(l => stdin.emit('log-line', l));
+process.stdin.on('data', data => {
+    const lines = data.toString()
+        .split(/[\r\n|\n]/);
+    
+    lines[0] = incompleteLine + lines[0];
+    incompleteLine = lines.pop(); // Either an incomplete line or an empty string due to the last \n
+
+    lines.filter(line => line.length > 0)
+        .forEach(line => stdin.emit('log-line', line));
 });
 
 module.exports.stdin = stdin;
